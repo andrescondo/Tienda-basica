f5638f9323e483151421d1817886b5f360636cda
andrescondo <1dkevinandres.b@gmail.com>
2021-07-03 19:32:46 -0500

    comunicación entre clases

--- Tienda/src/tienda/Tienda.java
+++ Tienda/src/tienda/Tienda.java
@@ -16,17 +16,15 @@ public class Tienda {
     /**
      * @param args the command line arguments
      */
+    // SE COMENTA CON CTRL + SHIT + C
+    
     public static void main(String[] args) {
         // TODO code application logic here
-        Scanner sc = new Scanner(System.in);
-        String name;
+        Scanner sc = new Scanner(System.in);//variable para ingreso por teclado
         
-        System.out.println("Ingresa un nombre");
+        Products product = new Products();//comunicacion con otro archivo del proyecto
         
-        name = sc.next();
-        
-        System.out.println(name);
-        
+        System.out.println(product);
     }
     
 }
