344ae5bfb7cc379c94edb54e7156c65d4e0713e1
andrescondo <1dkevinandres.b@gmail.com>
2021-07-06 12:49:33 -0500

    agregando nueva clase

---
+++ Tienda/src/tienda/Store.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tienda;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author AndresCondo @andreescondo
+ */
+public class Store { 
+
+    /**
+     * @param args the command line arguments
+     */
+    // SE COMENTA CON CTRL + SHIT + C
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);//variable para ingreso por teclado
+
+        boolean exit = true;
+        
+        while(exit){
+            System.out.println("1.- Vender");
+            System.out.println("2.- Ver Bodega");
+            System.out.println("3.- Comprar distribuidor");
+            System.out.println("4.- Salir");
+            
+            System.out.println("Eliga alguna opción:");
+            String option = sc.nextLine();
+            
+            switch(option){
+                case "1":
+                    System.out.println("Opcion uno");
+                    break;
+                case "2":
+                    System.out.println("Opcion dos");
+                    break;
+                    
+                case "4":
+                    exit = false;
+                    break;
+            }
+        }
+    }
+
+}
